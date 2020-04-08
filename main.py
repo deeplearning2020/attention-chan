117f06ce246892029e624db7bafbe81c04c7c633
goyeahia <yeahia.ruet@gmail.com>
2020-04-09 02:17:27 +0600

    gpu

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ from tensorflow.python.keras.preprocessing.image import load_img
 from tensorflow.python.keras import Input, optimizers
 from tensorflow.python.keras.models import Model
 from matplotlib import pyplot as plt
-from util import ConvATT, Conv
+from util import ConvATT, Conv, Deconv
 
 
 def model(inputShape):
     input_img = Input(shape=(inputShape))
     x = Conv(128, 3, strides = 1)(input_img)
     x = Conv(64, 3, strides = 1)(x)
+    x = Deconv(32,3)(x)
     x = Conv(32, 3, strides = 1)(x)
     x = ConvATT(32,3)(x)
     x = Conv(16,3, strides = 1)(x)
+    x = Deconv(16, 3)(x)
     x = ConvATT(16,3)(x)
     x = Conv(3, 3, strides = 1)(x)
     model = Model(input_img, x)
--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-from tensorflow.python.keras.layers import (Conv2D,
+from tensorflow.python.keras.layers import (Conv2D, Conv2DTranspose
             BatchNormalization, LeakyReLU)
 from tensorflow.python.keras.models import Model
 from layers import SelfAttention
@@ -27,6 +27,20 @@ class Conv(object):
     def __call__(self, x, training = None):
 
         x = Conv2D(self.filters, self.kernelSize, strides = self.strides, padding = 'same')(x)
+        x = SelfAttention(ch = self.filters)(x)
+        x = LeakyReLU()(x)
+        return x
+
+class DeConv(object):
+
+    def __init__(self, filters, kernelSize, strides = 2):
+        self.filters = filters
+        self.kernelSize = kernelSize
+        self.strides = strides
+
+    def __call__(self, x, training = None):
+
+        x = Conv2DTranspose(self.filters, self.kernelSize, strides = self.strides, padding = 'same')(x)
         x = SelfAttention(ch = self.filters)(x)
         x = LeakyReLU()(x)
         return x
