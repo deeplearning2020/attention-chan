5e7163e402a6c8eb495418b5b629bbe18a971370
goyeahia <yeahia.ruet@gmail.com>
2020-04-09 21:58:47 +0600

    despo

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ from util import Resnet_block
 
 def model(inputShape):
     input_img = Input(shape=(inputShape))
-    x = Conv_2D(256, 3, strides = 1)(input_img)
-    x = Conv_2D(128, 3, strides = 1)(x)
+    x = Conv_2D(128, 3, strides = 1)(input_img)
+    x = Conv_2D(64, 3, strides = 1)(x)
     for i in range(5):
-        x = Resnet_block(128, 3)(x)
-    x = Deconv(64,3, strides = 1)(x)
+        x = Resnet_block(64, 3)(x)
     x = Conv_2D(32, 3, strides = 1)(x)
     x = SelfAttention(ch = 32)(x)
     x = Deconv(16,3, strides = 1)(x)
