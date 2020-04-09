13fd3d9c00744d70604b1153e95e67fcf3d39933
goyeahia <yeahia.ruet@gmail.com>
2020-04-09 21:15:52 +0600

    hello friend

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ from util import Resnet_block
 
 def model(inputShape):
     input_img = Input(shape=(inputShape))
-    x = Conv_2D(128, 3, strides = 2)(input_img)
-    x = Conv_2D(32, 3, strides = 2)(x)
+    x = Conv_2D(1024, 3, strides = 1)(input_img)
+    x = Conv_2D(512, 3, strides = 1)(x)
+    x = Conv_2D(256, 3, strides = 1)(x)
+    x = Conv_2D(128, 3, strdies = 1)(x)
     for i in range(5):
-        x = Resnet_block(32, 3)(x)
-    x = Deconv(32,3, strides = 2)(x)
+        x = Resnet_block(128, 3)(x)
+    x = Deconv(64,3, strides = 2)(x)
     x = Conv_2D(32, 3, strides = 1)(x)
-    x = Deconv(16,3, strides = 2)(x)
-    x = SelfAttention(ch = 16)(x)
+    x = Deconv(16,3, strides = 1)(x)
     x = BatchNormalization()(x)
     x = Conv_2D(3, 3, strides = 1)(x)
     model = Model(input_img, x)
