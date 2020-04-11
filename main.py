707952a1274f0dcaed58104e8f7bb9082cb3a47d
goyeahia <yeahia.ruet@gmail.com>
2020-04-11 14:33:05 +0600

    lets

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def model(inputShape):
     input_img = Input(shape=(inputShape))
     x = Conv_2D(512, 3, strides = 1)(input_img)
     x = Conv_2D(128, 3, strides = 1)(x)
-    x = ChannelAttention(128, reduction = 1)(x)
+    x = Deconv(64, 3, strides = 1)(x)
+    x = ChannelAttention(64, reduction = 1)(x)
     for i in range(10):
-        x = Resnet_block(128, 3)(x)
+        x = Resnet_block(64, 3)(x)
     x = Conv_2D(32, 3, strides = 1)(x)
     x = ChannelAttention(32, reduction = 1)(x)
     x = Deconv(16, 3, strides = 1)(x)
