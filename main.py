693e58ae3d87e0bd7f7743a6ab69ce3ca30bc4bb
goyeahia <yeahia.ruet@gmail.com>
2020-04-12 12:48:43 +0600

    fixed size

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ def model(inputShape):
     x = MaxPooling2D((2, 2), padding='same')(x)
     x = Deconv(64, 3, strides = 2)(x)
     x = ChannelAttention(64, reduction = 1)(x)
+    x = SpatialAttention(64)(x)
     for i in range(5):
         x = Resnet_block(64, 3)(x)
-    x = Conv_2D(32, 5, strides = 1)(x)
+    x = Deconv(32, 5, strides = 2)(x)
     x = ChannelAttention(32, reduction = 1)(x)
     x = SpatialAttention(32)(x)
-    x = Deconv(16, 3, strides = 1)(x)
+    x = Deconv(16, 3, strides = 2)(x)
     x = Conv_2D(3, 3, strides = 1)(x)
     model = Model(input_img, x)
     return model
