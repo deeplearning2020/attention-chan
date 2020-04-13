6e22dc42c2bd4ce968e9111bb644b1a657fde171
goyeahia <yeahia.ruet@gmail.com>
2020-04-14 02:05:03 +0600

    asdf

--- main.py
+++ main.py
@@ -24,12 +24,13 @@ def model(inputShape):
     x = Conv_2D(512, 5, strides = 1)(x)
     x = Conv_2D(256, 5, strides = 1)(x)
     x = Conv_2D(128, 5, strides = 1)(x)
-    num_res_net_blocks = 20
+    num_res_net_blocks = 10
     for i in range(num_res_net_blocks):
         x = res_net_block(x, 128, 1)
     x = Conv_2D(64, 5, strides = 1)(x)
     x = Conv_2D(32, 5, strides = 1)(x)
     x = Conv_2D(16, 5, strides = 1)(x)
+    x = Conv_2D(8, 5, strides = 1)(x)
     x = Conv_2D(3, 3, strides = 1)(x)
     model = Model(input_img, x)
     return model
