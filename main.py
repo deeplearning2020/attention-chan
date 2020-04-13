88cf6d82ff1846c0842be916caff2ef7998be057
goyeahia <yeahia.ruet@gmail.com>
2020-04-13 13:15:58 +0600

    pls

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D, UpSampling2D, GaussianNoise, LeakyReLU
 from tensorflow.keras.optimizers import Adam
 from matplotlib import pyplot as plt
-from util import ConvATT, Conv_2D, simple_conv
+from util import Conv_2D, simple_conv, Deconv
 from layers import SelfAttention
 from attention_layer import ChannelAttention, SpatialAttention
 from util import Resnet_block
@@ -21,9 +21,17 @@ from util import Resnet_block
 def model(inputShape):
     input_img = Input(shape=(inputShape))
     x = simple_conv(128, 3, strides = 1,)(input_img)
+    x = Deconv(128, 3, strides = 1)(x)
+    x = Conv_2D(128, 3, strides = 1)(x)
     x = simple_conv(64, 3 , strides = 1)(x)
+    x = Deconv(64, 3, strides = 1)(x)
+    x = Conv_2D(64, 3, strides = 1)(x)
     x = simple_conv(32, 3, strides = 1)(x)
-    x = SelfAttention(ch = 32)(x)
+    x = Deconv(32, 3, strides = 1)(x)
+    x = Conv_2D(32, 3, strides = 1)(x)
+    x = simple_conv(16, 3, strides = 1)(x)
+    x = Deconv(16, 3, strides = 1)(x)
+    x = Conv_2D(16, 3, strides = 1)(x)
     #x = SpatialAttention(128)(x)
     """
     x = Conv_2D(128, 5, strides = 1)(x)
@@ -74,7 +82,7 @@ def main():
             patience = 25) ## early stopping to prevent overfitting
 
     history = nn.fit(lr_image, hr_image,
-                epochs = 600,
+                epochs = 1000,
                 batch_size = batchSize, callbacks = [es])
 
     """ reconstrucing high-resolution image from the low-resolution image """
--- util.py
+++ util.py
@@ -49,21 +49,6 @@ class Resnet_block(object):
 
 
 
-class ConvATT(object):
-
-    def __init__(self, filters, kernelSize, strides = 2):
-        self.filters = filters
-        self.kernelSize = kernelSize
-        self.strides = strides
-
-    def __call__(self, x, training = None):
-
-        x = Conv2D(self.filters, self.kernelSize, strides = self.strides, padding = 'same')(x)
-        x = SelfAttention(ch = self.filters)(x)
-        x = BatchNormalization()(x)
-        x = LeakyReLU()(x)
-        return x
-
 class Conv_2D(object):
 
     def __init__(self, filters, kernelSize, strides = 1):
@@ -74,6 +59,21 @@ class Conv_2D(object):
     def __call__(self, x, training = None):
 
         x = SeparableConv2D(self.filters, self.kernelSize, strides = self.strides, padding = 'same')(x)
+        x = BatchNormalization()(x)
+        x = LeakyReLU()(x)
+        return x
+
+
+class Deconv(object):
+
+    def __init__(self, filters, kernelSize, strides = 1):
+        self.filters = filters
+        self.kernelSize = kernelSize
+        self.strides = strides
+
+    def __call__(self, x, training = None):
+
+        x = Conv2DTranspose(self.filters, self.kernelSize, strides = self.strides, padding = 'same')(x)
         x = BatchNormalization()(x)
         x = LeakyReLU()(x)
         return x
