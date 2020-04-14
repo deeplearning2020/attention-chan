c0901083b26d5f7711aa4183fd6b3d99877dcb17
goyeahia <yeahia.ruet@gmail.com>
2020-04-14 17:58:48 +0600

    increase epochs

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
             patience = 700) ## early stopping to prevent overfitting
 
     history = nn.fit(lr_image, hr_image,
-                epochs = 2000,
+                epochs = 3000,
                 batch_size = batchSize, callbacks = [es])
 
     """ reconstrucing high-resolution image from the low-resolution image """
