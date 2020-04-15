37ae35a492e0f865e78dffa16702846f66ae4741
goyeahia <yeahia.ruet@gmail.com>
2020-04-15 23:30:42 +0600

    may be perfect

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
             patience = 700) ## early stopping to prevent overfitting
 
     history = nn.fit(lr_image, hr_image,
-                epochs = 3000,
+                epochs = 4000,
                 batch_size = batchSize, callbacks = [es])
 
     """ reconstrucing high-resolution image from the low-resolution image """
