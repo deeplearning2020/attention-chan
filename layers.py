cdb17bad58ef441ee9c72919e9d2f2496fc07df0
goyeahia <yeahia.ruet@gmail.com>
2020-04-15 12:37:46 +0600

    fixing

--- layers.py
+++ layers.py
@@ -23,19 +23,17 @@ class AttentionBlock(object):
         x1 = BatchNormalization()(x1)
 
         g2 = Conv2D(self.filters, kernel_size = 1)(x) 
-        g2 = BatchNormalization()(g1)
+        g2 = BatchNormalization()(g2)
 
         x2 = Conv2D(self.filters, kernel_size = 1)(x) 
-        x2 = BatchNormalization()(x1)
+        x2 = BatchNormalization()(x2)
 
         g3 = Conv2D(self.filters, kernel_size = 1)(x) 
-        g3 = BatchNormalization()(g1)
+        g3 = BatchNormalization()(g3)
 
-        x3 = Conv2D(self.filters, kernel_size = 1)(x) 
-        x3 = BatchNormalization()(x1)
         
-        g1_x1 = Add()([g1, x1,g2, x2,g3, x3])
-        psi = Activation('relu')(g1_x1)
+        g1_x1 = Add()([g1, x1, g2, x2, g3])
+        psi = LeakyReLU()(g1_x1)
 
         psi = Conv2D(1,kernel_size = 1)(psi) 
         psi = BatchNormalization()(psi)
