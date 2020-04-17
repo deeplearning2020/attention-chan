7e15536a57df993241eb56d4b1c412abbd62ca0c
goyeahia <yeahia.ruet@gmail.com>
2020-04-17 13:18:06 +0600

    fixing

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def main():
     nn = model(inputShape)
     print(nn.summary())
     optimizer = Adam(lr=1e-2, epsilon = 1e-8, beta_1 = .9, beta_2 = .999)
-    nn.compile(optimizer = optimizer, loss = 'mse')
+    nn.compile(optimizer = optimizer, loss = adv_loss)
     
     es = EarlyStopping(monitor = adv_loss , mode = 'min', verbose = 1, 
             patience = 700) ## early stopping to prevent overfitting
