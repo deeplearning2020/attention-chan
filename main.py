c22d1b2dccc4c1f09b1b7b1434f7cd1febd253b3
goyeahia <yeahia.ruet@gmail.com>
2020-04-18 15:16:47 +0600

    increases patience

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main():
     nn.compile(optimizer = optimizer, loss = 'mse')
     
     es = EarlyStopping(monitor = 'loss' , mode = 'min', verbose = 1, 
-            patience = 25) ## early stopping to prevent overfitting
+            patience = 1000) ## early stopping to prevent overfitting
 
     history = nn.fit(lr_image, hr_image,
                 epochs = 5000,
