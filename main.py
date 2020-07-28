34f44b4573c7eef03574bb0a2419c88f55e5ca1c
deeplearning2020 <yeahia.ruet@gmail.com>
2020-07-29 02:54:07 +0600

    sdasd

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def main():
             patience = 100) ## early stopping to prevent overfitting
 
     history = nn.fit(hr_image, lr_image,
-                epochs = 2000,
+                epochs = 1000,
                 batch_size = batchSize, callbacks = [es])
 
     """ reconstrucing high-resolution image from the low-resolution image """
